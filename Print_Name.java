66f553e305aec89386d678f681bb482d814f1796
K4fle D. Manish <k4fledmanish@aol.com>
2016-11-16 19:51:29 +0545

    Update Print_Name.java

--- Print_Name.java
+++ Print_Name.java
@@ -1,7 +1,5 @@
-class Print_Name
-{
-  public static void main(String[] args)
-  {
+class Print_Name{
+  public static void main(String[] args){
     System.out.println("Hello! This is K4fle D. Manish")
   }
 } 
