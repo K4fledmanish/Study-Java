cad52e4b46b995afbcfa6af1760c01618c445328
K4fle D. Manish <k4fledmanish@aol.com>
2016-11-18 17:51:44 +0545

    Update Calculation_at_once.java

--- Calculation_at_once.java
+++ Calculation_at_once.java
@@ -1,11 +1,11 @@
 class Calculation_at_once{
   public static void main(String[] args){
     int a=10;
-    int b=5;
-    int c=a+b;
-    int d=c-b;
-    int e =d*a;
-    int f=e/b;
+    int b=5;                                                                      Output:
+    int c=a+b;                                                                    The value is 15
+    int d=c-b;                                                                    The value is 10
+    int e =d*a;                                                                   The value is 100
+    int f=e/b;                                                                    The value is 20
       System.out.println("The value is "+c);
       System.out.println("The value is "+d);
       System.out.println("The value is "+e);
@@ -18,11 +18,11 @@ If You want to do calculation in decimal then :
 class Calculation_at_once{
   public static void main(String[] args){
     Double a=10;
-    Double b=5;
-    Double c=a+b;
-    Double d=c-b;
-    Double e =d*a;
-    Double f=e/b;
+    Double b=5;                                                                   Output:
+    Double c=a+b;                                                                 The value is 15.0
+    Double d=c-b;                                                                 The value is 10.0
+    Double e =d*a;                                                                The value is 100.0
+    Double f=e/b;                                                                 The value is 20.0
       System.out.println("The value is "+c);
       System.out.println("The value is "+d);
       System.out.println("The value is "+e);
