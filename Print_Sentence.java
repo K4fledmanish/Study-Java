7c0fe573559bf1717db19741e67e01a1e9dde2b0
K4fle D. Manish <k4fledmanish@aol.com>
2016-11-18 15:37:43 +0545

    Create Print_Sentence.java

---
+++ Print_Sentence.java
@@ -0,0 +1,7 @@
+class Print_Sentence{
+  public ststic void main(String[] args){
+    System.out.println("Hello World");
+    System.out.println("This is a basic programming of Java");
+    System.out.println("Visit often in this account for Java Lessons");
+ }
+} 
