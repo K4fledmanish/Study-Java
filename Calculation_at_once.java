498b15431b5f814163547e800b783a703e712fbd
K4fle D. Manish <k4fledmanish@aol.com>
2016-11-19 11:02:49 +0545

    Update Calculation_at_once.java

--- Calculation_at_once.java
+++ Calculation_at_once.java
@@ -1,15 +1,14 @@
 class Calculation_at_once{
   public static void main(String[] args){
     int a=10;
-    int b=5;                                                                      Output:
-    int c=a+b;                                                                    The value is 15
-    int d=c-b;                                                                    The value is 10
-    int e =d*a;                                                                   The value is 100
-    int f=e/b;                                                                    The value is 20
-      System.out.println("The value is "+c);
-      System.out.println("The value is "+d);
-      System.out.println("The value is "+e);
-      System.out.println("The value is "+f);
+    int b=5;                                                                      
+    int c=a+b;                                                                    
+    int e =d*a;                                                                   
+    int f=e/b;                                                                    Output:
+      System.out.println("The value is "+c);                                      The value is 15
+      System.out.println("The value is "+d);                                      The value is 10
+      System.out.println("The value is "+e);                                      The value is 20
+      System.out.println("The value is "+f);                                      The value is 100
  }
 } 
 
@@ -18,14 +17,14 @@ If You want to do calculation in decimal then :
 class Calculation_at_once{
   public static void main(String[] args){
     Double a=10;
-    Double b=5;                                                                   Output:
-    Double c=a+b;                                                                 The value is 15.0
-    Double d=c-b;                                                                 The value is 10.0
-    Double e =d*a;                                                                The value is 100.0
-    Double f=e/b;                                                                 The value is 20.0
-      System.out.println("The value is "+c);
-      System.out.println("The value is "+d);
-      System.out.println("The value is "+e);
-      System.out.println("The value is "+f);
+    Double b=5;                                                                   
+    Double c=a+b;                                                                 
+    Double d=c-b;                                                                 
+    Double e =d*a;                                                                
+    Double f=e/b;                                                                 Output:
+      System.out.println("The value is "+c);                                      The value is 15.0
+      System.out.println("The value is "+d);                                      The value is 10.0
+      System.out.println("The value is "+e);                                      The value is 100.0
+      System.out.println("The value is "+f);                                      The value is 20.0
  }
 } 
